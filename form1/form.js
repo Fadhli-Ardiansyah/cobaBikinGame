89ea50b7cf8984f5da1fca6488f93b1fc4c7d469
Fadhli-Ardiansyah <oradila9@gmail.com>
2024-02-13 01:07:12 +0700

    ui baru

--- form1/form.js
+++ form1/form.js
@@ -1,26 +1,30 @@
+const reset = document.getElementById("reset");
 const pembungkus = document.getElementById("Pembungkus");
 const merah = document.querySelector("input[name=merah]");
 const hijau = document.querySelector("input[name=hijau]");
 const biru = document.querySelector("input[name=biru]");
 const jawaban = document.getElementById("jawaban");
 const soal = document.getElementById("soal");
-soal.classList.toggle("Te");
 console.log(pembungkus);
 
 const btn1 = document.getElementById("bebek");
 const btn2 = document.createElement("button");
 
+reset.addEventListener("click", () => {
+  location.reload();
+});
+
 btn1.addEventListener("click", () => {
   btn2.id = "btn2";
   btn2.innerHTML = "testt";
+
   pembungkus.appendChild(btn2);
 
   const r = Math.round(Math.random() * 255 + 1);
   const g = Math.round(Math.random() * 255 + 1);
   const b = Math.round(Math.random() * 255 + 1);
-
+  console.log("nambah terus");
   soal.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-  console.log(r);
   if (btn1 ?? soal) {
     merah.addEventListener("input", () => {
       let r = merah.value;
@@ -43,17 +47,18 @@ btn1.addEventListener("click", () => {
       let g = hijau.value;
       let b = biru.value;
       jawaban.style.backgroundColor = `rgb(${r} ${g} ${b})`;
-      console.log(r);
-    });
-
-    btn2.addEventListener("click", () => {
-      if (r == merah.value) {
-        alert("selamat anda benar");
-      } else {
-        alert("sayang sekali anda gagal");
-      }
     });
   } else {
     alert("gagal");
   }
+  btn2.addEventListener("click", () => {
+    if (r == merah.value) {
+      alert("selamat anda benar");
+    } else {
+      alert("sayang sekali anda gagal");
+      btn2.style.backgroundColor = "red";
+      btn2.style.padding = "8px 13px";
+      btn2.innerHTML = "✕";
+    }
+  });
 });
